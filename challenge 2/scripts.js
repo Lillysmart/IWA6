761616a5541359ab693ff099f4171bde89fe67c3
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-08-10 09:39:09 +0200

    Update scripts.js

--- challenge 2/scripts.js
+++ challenge 2/scripts.js
@@ -3,15 +3,25 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
+const hourOfDay = 0;
+const minuteOfDay = 0;
 
 // Only change below this line
+let balance= null
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
-}
-	
-console.log(balance.toFixed(3))
+if((hourOfDay === 0) && (minuteOfDay === 0))  {
+	const taxAsDecimal = parseInt(tax) /100
+    console.log (taxAsDecimal) 
+
+    let finalTax =  taxAsDecimal *salary //8% of salary
+    console.log (finalTax)
+
+  const startingAfterTax = salary - finalTax
+  console.log(startingAfterTax)
+    
+ balance = startingAfterTax -transport - food - rent
+
+console.log("R"+ balance.toFixed(2)) 
+} else{
+    console.log('No balance found')
+ }
